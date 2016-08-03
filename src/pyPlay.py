816ba60a5a0ce863d719b64c088a62118ef5c0cd
Steven Lieblich <steven.lieblich@gmail.com>
2016-08-02 22:26:18 -0400

    Added some pandas code

--- src/pyPlay.py
+++ src/pyPlay.py
@@ -1,6 +1,8 @@
 import itertools, random
+import numpy as np
+import pandas as pd
 
-# make a deck of cards
+# basic use of itertools to make and shuffle a deck of cards
 deck = list(itertools.product(['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
                                'Jack', 'Queen', 'King'],
                               ['Spades', 'Hearts', 'Clubs', 'Diamonds']))
@@ -11,4 +13,15 @@ random.shuffle(deck)
 # draw five cards
 print ("You got:")
 for i in range(5):
-    print "%s of %s" % (deck[i][0], deck[i][1])
+    print "\t %s of %s" % (deck[i][0], deck[i][1])
+
+
+# playing around with pandas
+data = {
+    'Name': ["John", "Paul", "George", "Ringo"],
+    'Location': ["NY", "LN", "HK", "TK"],
+    'Age': [22, 23, 24, 25]
+}
+
+dp = pd.DataFrame(data)
+print dp
